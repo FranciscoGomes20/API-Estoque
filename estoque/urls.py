5ffa3cf368b81d4de5690952d8ad4b879c1c2167
FranciscoGomes20 <henriquecgomes10@gmail.com>
2024-05-24 14:00:19 -0500

    Get, put and delete functions have been added to views

--- estoque/urls.py
+++ estoque/urls.py
@@ -1,13 +1,38 @@
 from django.urls import path
-from .views import CategoriaAPIView, UnidadeAPIView, FornecedorAPIView, FuncionarioAPIView, MotivoAPIView, ProdutoAPIView, SaidaAPIView, SaidaHasProdutoAPIView
+from .views import (
+        CategoriaAPIView, 
+        CategoriaAPIViewID, 
+        UnidadeAPIView, 
+        UnidadeAPIViewID, 
+        FornecedorAPIView, 
+        FornecedorAPIViewID, 
+        FuncionarioAPIView,
+        FuncionarioAPIViewID,
+        MotivoAPIView,
+        MotivoAPIViewID,
+        ProdutoAPIView,
+        ProdutoAPIViewID,
+        SaidaAPIView,
+        SaidaAPIViewID,
+        SaidaHasProdutoAPIView,
+        SaidaHasProdutoAPIViewID
+    )
 
 urlpatterns = [
     path('categorias/', CategoriaAPIView.as_view(), name='categorias'),
+    path('categoria/<int:id>/', CategoriaAPIViewID.as_view(), name='categoria'),
     path('fornecedores/', FornecedorAPIView.as_view(), name='fornecedores'),
+    path('fornecedor/<int:id>/', FornecedorAPIViewID.as_view(), name='fornecedor'),
     path('funcionarios/', FuncionarioAPIView.as_view(), name='funcionarios'),
+    path('funcionario/<int:id>/', FuncionarioAPIViewID.as_view(), name='funcionario'),
     path('motivos/', MotivoAPIView.as_view(), name='motivos'),
+    path('motivo/<int:id>/', MotivoAPIViewID.as_view(), name='motivo'),
     path('produtos/', ProdutoAPIView.as_view(), name='produtos'),
+    path('produto/<int:id>/', ProdutoAPIViewID.as_view(), name='produto'),
     path('saidas/', SaidaAPIView.as_view(), name='saidas'),
+    path('saida/<int:id>/', SaidaAPIViewID.as_view(), name='saida'),
     path('unidades/', UnidadeAPIView.as_view(), name='unidades'),
-    path('saidasdeprodutos/', SaidaHasProdutoAPIView.as_view(), name='saidasdeprodutos')
+    path('unidade/<int:id>/', UnidadeAPIViewID.as_view(), name='unidade'),
+    path('saidasdeprodutos/', SaidaHasProdutoAPIView.as_view(), name='saidasdeprodutos'),
+    path('saidasdeproduto/<int:id>/', SaidaHasProdutoAPIViewID.as_view(), name='saidasdeproduto')
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -17,6 +17,24 @@ class CategoriaAPIView(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+class CategoriaAPIViewID(APIView):
+    def get(self, request, id):
+        categoria = Categoria.objects.get(pk=id)
+        serializer = CategoriaSerializer(categoria)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        categoria = Categoria.objects.get(pk=id)
+        serializer = CategoriaSerializer(categoria, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, id):
+        categoria = Categoria.objects.get(pk=id)
+        categoria.delete()
+        return Response(status=status.HTTP_200_OK)
+
 class UnidadeAPIView(APIView):
     def get(self, request):
         unidades = Unidade.objects.all()
@@ -29,10 +47,28 @@ class UnidadeAPIView(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+class UnidadeAPIViewID(APIView):
+    def get(self, request, id):
+        unidade = Unidade.objects.get(pk=id)
+        serializer = UnidadeSerializer(unidade)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        unidade = Unidade.objects.get(pk=id)
+        serializer = UnidadeSerializer(unidade, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, id):
+        unidade = Unidade.objects.get(pk=id)
+        unidade.delete()
+        return Response(status=status.HTTP_200_OK)
+
 class FornecedorAPIView(APIView):
     def get(self, request):
-        fornecedor = Fornecedor.objects.all()
-        serialiser = FornecedorSerializer(fornecedor, many=True)
+        fornecedores = Fornecedor.objects.all()
+        serialiser = FornecedorSerializer(fornecedores, many=True)
         return Response(serialiser.data)
 
     def post(self, reguest):
@@ -40,11 +76,29 @@ class FornecedorAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+class FornecedorAPIViewID(APIView):
+    def get(self, request, id):
+        fornecedor = Fornecedor.objects.get(pk=id)
+        serializer = FornecedorSerializer(fornecedor)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        fornecedor = Fornecedor.objects.get(pk=id)
+        serializer = FornecedorSerializer(fornecedor, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, id):
+        fornecedor = Fornecedor.objects.get(pk=id)
+        fornecedor.delete()
+        return Response(status=status.HTTP_200_OK)
 
 class FuncionarioAPIView(APIView):
     def get(self, request):
-        funcionario = Funcionario.objects.all()
-        serializer = FuncionarioSerializer(funcionario, many=True)
+        funcionarios = Funcionario.objects.all()
+        serializer = FuncionarioSerializer(funcionarios, many=True)
         return Response(serializer.data)
 
     def post(self, reguest):
@@ -53,10 +107,28 @@ class FuncionarioAPIView(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+class FuncionarioAPIViewID(APIView):
+    def get(self, request, id):
+        funcionario = Funcionario.objects.get(pk=id)
+        serializer = FuncionarioSerializer(funcionario)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        funcionario = Funcionario.objects.get(pk=id)
+        serializer = FuncionarioSerializer(funcionario, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, id):
+        funcionario = Funcionario.objects.get(pk=id)
+        funcionario.delete()
+        return Response(status=status.HTTP_200_OK)
+
 class MotivoAPIView(APIView):
     def get(self, request):
-        motivo = Motivo.objects.all()
-        serializer = MotivoSerializer(motivo, many=True)
+        motivos = Motivo.objects.all()
+        serializer = MotivoSerializer(motivos, many=True)
         return Response(serializer.data)
     
     def post(self, reguest):
@@ -64,6 +136,24 @@ class MotivoAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class MotivoAPIViewID(APIView):
+    def get(self, request, id):
+        motivo = Motivo.objects.get(pk=id)
+        serializer = MotivoSerializer(motivo)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        motivo = Motivo.objects.get(pk=id)
+        serializer = MotivoSerializer(motivo, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, id):
+        motivo = Motivo.objects.get(pk=id)
+        motivo.delete()
+        return Response(status=status.HTTP_200_OK)
 
 class ProdutoAPIView(APIView):
     def get(self, request):
@@ -76,6 +166,24 @@ class ProdutoAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+class ProdutoAPIViewID(APIView):
+    def get(self, request, id):
+        produto = Produto.objects.get(pk=id)
+        serializer = ProdutoSerializer(produto)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        produto = Produto.objects.get(pk=id)
+        serializer = ProdutoSerializer(produto, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, id):
+        produto = Produto.objects.get(pk=id)
+        produto.delete()
+        return Response(status=status.HTTP_200_OK)
 
 class SaidaAPIView(APIView):
     def get(self, request):
@@ -89,6 +197,24 @@ class SaidaAPIView(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+class SaidaAPIViewID(APIView):
+    def get(self, request, id):
+        saida = Saida.objects.get(pk=id)
+        serializer = SaidaSerializer(saida)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        saida = Saida.objects.get(pk=id)
+        serializer = SaidaSerializer(saida, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, id):
+        saida = Saida.objects.get(pk=id)
+        saida.delete()
+        return Response(status=status.HTTP_200_OK)
+
 class SaidaHasProdutoAPIView(APIView):
     def get(self, request):
         saidahasproduto = SaidaHasProduto.objects.all()
@@ -100,3 +226,21 @@ class SaidaHasProdutoAPIView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class SaidaHasProdutoAPIViewID(APIView):
+    def get(self, request, id):
+        saidahasproduto = SaidaHasProduto.objects.get(pk=id)
+        serializer = SaidaHasProdutoSerealizer(saidahasproduto)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        saidahasproduto = SaidaHasProduto.objects.get(pk=id)
+        serializer = SaidaHasProdutoSerealizer(saidahasproduto, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, id):
+        saidahasproduto = SaidaHasProduto.objects.get(pk=id)
+        saidahasproduto.delete()
+        return Response(status=status.HTTP_200_OK)
